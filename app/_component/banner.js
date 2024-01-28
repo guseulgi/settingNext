403822d146603ce36f0e5e8cdc9a23a990f8c672
seulgi <guseul9405@gmail.com>
2024-01-29 05:54:17 +0900

    Feat: 반응형 배너 완

--- app/_component/banner.js
+++ app/_component/banner.js
@@ -15,37 +15,32 @@ export default function PopBanner() {
     <div
       id="marketing-banner"
       tabindex="-1"
-      className={`${close} z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600`}
+      className={`${close} z-50 flex flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600`}
     >
-      <div className="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600"
+      <div
+        href="https://flowbite.com/"
+        className="hidden md:flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600"
+      >
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 21"
         >
-          <svg
-            class="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 21"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C17 15.4 17 16 16.462 16H3.538C3 16 3 15.4 3 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 10 3.464ZM1.866 8.832a8.458 8.458 0 0 1 2.252-5.714m14.016 5.714a8.458 8.458 0 0 0-2.252-5.714M6.54 16a3.48 3.48 0 0 0 6.92 0H6.54Z"
-            />
-          </svg>
-          {/* <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
-            Moncolle
-          </span> */}
-        </a>
-        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-          포켓몬스터 썬&문 종방 기념! 새로운 몬콜레 시리즈 출시!
-        </p>
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C17 15.4 17 16 16.462 16H3.538C3 16 3 15.4 3 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 10 3.464ZM1.866 8.832a8.458 8.458 0 0 1 2.252-5.714m14.016 5.714a8.458 8.458 0 0 0-2.252-5.714M6.54 16a3.48 3.48 0 0 0 6.92 0H6.54Z"
+          />
+        </svg>
       </div>
-      <div className="flex items-center flex-shrink-0">
+      <p className="flex flex-1 items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+        포켓몬스터 썬&문 종방 기념! 새로운 몬콜레 시리즈 출시!
+      </p>
+      <div className="flex items-center justify-end">
         <Link
           href="/shop"
           className="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
