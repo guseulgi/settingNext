76a049174c03befb535c0d81c9bfb1b2e9f0a58e
seulgi <guseul9405@gmail.com>
2024-02-01 06:17:01 +0900

    Feat: 마이페이지 구성 중

--- app/_variable/menu.js
+++ app/_variable/menu.js
@@ -1,21 +1,21 @@
 export const menu = [
   {
-    name: "Notice",
+    name: "공지사항",
     href: "/notice",
     alt: "공지사항",
   },
   {
-    name: "Moncolle?",
+    name: "몬콜레?",
     href: "/main",
     alt: "몬콜레에 대한 설명",
   },
   {
-    name: "List",
+    name: "목록",
     href: "/list",
     alt: "전체 리스트",
   },
   {
-    name: "Get cha!",
+    name: "넌 내거야!",
     href: "/getcha",
     alt: "오늘의 포켓몬",
   },
@@ -25,8 +25,15 @@ export const menu = [
     alt: "",
   },
   {
-    name: "MyPage",
+    name: "마이페이지",
     href: "/mypage",
     alt: "마이페이지",
   },
+];
+
+export const myPageMenu = [
+  { title: "개인정보수정", link: "/mypage/profile" },
+  { title: "비밀번호 변경", link: "/mypage/changepw" },
+  { title: "장바구니", link: "/mypage/mycart" },
+  { title: "좋아요 목록", link: "/mypage/likelist" },
 ];
--- app/notice/@modal/page.js
+++ app/notice/@modal/page.js
@@ -1,5 +1,10 @@
 import Modal from "@/app/_component/modal";
 
 export default function NoticePage() {
-  return <Modal content="I'm preparing this section" noA="Close" />;
+  return (
+    <Modal
+      content="해당 페이지는 준비중입니다. 혼란을 드려 죄송합니다."
+      noA="Close"
+    />
+  );
 }
