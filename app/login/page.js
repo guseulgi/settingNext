357361d74a13b19bf4ce3672f8aeb66d5fb31281
seulgi <guseul9405@gmail.com>
2024-02-10 21:52:31 +0900

    Feat: 로그인/회원가입 디테일 수정

--- app/login/page.js
+++ app/login/page.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Login() {
   return (
     <main className="flex flex-col items-center my-5 md:w-8/12 mx-auto">
-      <h2 className="mx-auto text-2xl my-7">Login, play with us!</h2>
+      <h2 className="mx-auto text-2xl my-7">함께 놀자!</h2>
 
       <form className="flex flex-col gap-4 mb-2 w-1/2">
         <div className="block">
@@ -21,7 +21,7 @@ export default function Login() {
               <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"></path>
               <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"></path>
             </svg>
-            <Label htmlFor="email" value="Email" />
+            <Label htmlFor="email" value="이메일" />
           </div>
           <TextInput
             id="email1"
@@ -34,26 +34,26 @@ export default function Login() {
 
         <div className="block">
           <div className="mb-2">
-            <Label htmlFor="password" value="Password" />
+            <Label htmlFor="password" value="비밀번호" />
           </div>
           <TextInput id="password1" type="password" required color="blue" />
         </div>
 
         <div className="flex items-center gap-2 mb-2">
           <Checkbox id="remember" color="blue" />
-          <Label htmlFor="remember">Remember me</Label>
+          <Label htmlFor="remember">로그인 정보 기억하기</Label>
         </div>
         <Button type="submit" color="blue">
-          Login
+          로그인
         </Button>
       </form>
 
       <div className="w-1/2 flex text-xs justify-around ">
         <div className="hover:font-bold">
-          <Link href="/signin">Register</Link>
+          <Link href="/signin">회원가입</Link>
         </div>
         <div className="hover:font-bold">
-          <Link href="/findpw">Find password</Link>
+          <Link href="/findpw">비밀번호를 잊으셨나요?</Link>
         </div>
       </div>
     </main>
--- app/signin/page.js
+++ app/signin/page.js
@@ -3,6 +3,9 @@
 import { Label, TextInput, Checkbox, Button } from "flowbite-react";
 import Link from "next/link";
 import { useState } from "react";
+import { Toast } from "flowbite-react";
+import { FaTelegramPlane } from "react-icons/fa";
+import { HiFire } from "react-icons/hi";
 
 const errMsg = ["Only input english", "Do not blink this field"];
 
@@ -34,10 +37,28 @@ export default function Signin() {
 
   return (
     <form className="flex flex-col gap-6 my-5">
-      <h2 className="mx-auto text-2xl mt-6 mb-3">Register, play with us!</h2>
+      <h2 className="mx-auto text-2xl mt-6 mb-3">
+        지금 가입하면 다양한 헤택이!
+      </h2>
+
+      <div className="flex justify-around gap-4 mx-20">
+        <Toast>
+          <HiFire className="h-5 w-5 text-cyan-300 dark:text-cyan-500" />
+          <div className="pl-4 text-sm font-normal">몬콜레 지원금 2,000원</div>
+        </Toast>
+        <Toast>
+          <HiFire className="h-5 w-5 text-cyan-500 dark:text-cyan-500" />
+          <div className="pl-4 text-sm font-normal">등급에 따른 할인</div>
+        </Toast>
+        <Toast>
+          <HiFire className="h-5 w-5 text-cyan-700 dark:text-cyan-500" />
+          <div className="pl-4 text-sm font-normal">포인트 적립과 쿠폰까지</div>
+        </Toast>
+      </div>
+
       <div className="w-1/2 md:w-1/3 items-center mx-auto">
         <div className="mb-2 block ">
-          <Label htmlFor="username" value="Username" />
+          <Label htmlFor="username" value="닉네임" />
         </div>
         <TextInput
           id="username"
@@ -61,7 +82,7 @@ export default function Signin() {
             <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"></path>
             <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"></path>
           </svg>
-          <Label htmlFor="email" value="Email" />
+          <Label htmlFor="email" value="이메일" />
         </div>
         <TextInput
           id="email"
@@ -74,7 +95,7 @@ export default function Signin() {
 
       <div className="w-1/2 md:w-1/3 items-center mx-auto">
         <div className="mb-2 block">
-          <Label htmlFor="password" value="Password" />
+          <Label htmlFor="password" value="비밀번호" />
         </div>
         <TextInput
           id="password1"
@@ -88,7 +109,7 @@ export default function Signin() {
 
       <div className="w-1/2 md:w-1/3 items-center mx-auto">
         <div className="mb-2 block">
-          <Label htmlFor="repeat-password" value="Repeat password" />
+          <Label htmlFor="repeat-password" value="비밀번호 확인" />
         </div>
         <TextInput
           id="repeat-password"
@@ -103,20 +124,21 @@ export default function Signin() {
         <div className="flex items-center gap-2 mt-1">
           <Checkbox id="agree" color="blue" />
           <Label htmlFor="agree" className="flex">
-            I agree with the&nbsp;
+            헤택을 담은&nbsp;
             <Link
               href="#"
               className="text-blue-700 hover:underline dark:text-blue-800"
             >
-              terms and conditions
+              이메일 수신
             </Link>
+            을 동의합니다.
           </Label>
         </div>
       </div>
 
       <div className="w-1/2 md:w-1/3 items-center mx-auto">
         <Button type="submit" className="w-full" color="blue">
-          Register new account
+          가입을 완료합니다
         </Button>
       </div>
     </form>
